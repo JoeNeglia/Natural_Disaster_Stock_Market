87624d06c144a515ab7969b7321a0a144df9cd34
Jack Wilson <jack.wilson.east@gmail.com>
2026-09-16 18:16:45 -0600

    added link to SP500 dataset

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-### Evaluating Impacts of Natural Disasters on the U.S. Stock Market
+# Evaluating Impacts of Natural Disasters on the U.S. Stock Market
 
-
+[OneDrive Link To S&P 500 Dataset](
+https://o365coloradoedu-my.sharepoint.com/:u:/r/personal/jawi2043_colorado_edu/Documents/SP500Last25Years.zip?d=w9fbdb7cb8b1447239bf8ffff2da58a65&csf=1&web=1&e=z5ojiI)
--- StockMarket.py
+++ StockMarket.py
@@ -2,21 +2,25 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-
-def get_stock_market_data(start_date, end_date):
-    # data = yf.download(
-    #     tickers,
-    #     start="2020-01-01",
-    #     end="2026-01-01",
-    #     interval="1d"
-    # )
+def get_stock_market_data(tickers, start_date, end_date):
+    data = yf.download(
+            tickers,
+            start="start_date",
+            end="end_date",
+            interval="1d"
+        )
 
     return
 
-def get_tickers():
+def get_all_tickers():
     df = pd.read_csv('sec_company_tickers.csv')
     return df["ticker"].tolist()
+
+def get_SP_500_tickers():
+    df = pd.read_csv('SP500Metadata.csv')
+    return df["Symbol"].tolist()
 
 def main():
     # TODO: Read in hurricanes.csv, filter for USA, get stock market data for each ticker around time period of hurricanes
@@ -24,9 +28,8 @@ def main():
     # df_usa = df[df['location'].str.contains('USA', na=False)]
 
     
-    print(get_tickers())
-
-
+    print(get_SP_500_tickers())
+    get_stock_market_data(get_SP_500_tickers()[0:5], "2026-01-01", "2026-02-01")
 
 
     return
